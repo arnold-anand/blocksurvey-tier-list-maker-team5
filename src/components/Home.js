df20772f546adf8bf49225a59b0f8fc014ec8741
arnold-anand <pagadalaarnold@karunya.edu.in>
2024-02-26 16:46:09 +0530

    logout addded and dashboard route configured

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { nhost } from "../lib/nhost";
 
 export default function Home() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
   return (
     <div>
       {/* Top bar */}
@@ -11,12 +18,40 @@ export default function Home() {
             <button className="bg-black h-[39px] w-[139px] text-white rounded-[8px] font-open-sans text-[14px]">
               Create Tieron
             </button>
-            <button className="bg-white p-[3px] rounded-full">
+            <button
+              onClick={toggleDropdown}
+              className="bg-white p-[3px] rounded-full"
+            >
               <img
                 className="w-[38px] h-[38px] rounded-full"
                 src="profile.png"
                 alt=""
               />
+              {dropdownVisible && (
+                <div className="absolute mt-2 right-3 w-52 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                  <div
+                    className="py-1"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="options-menu"
+                  >
+                    <Link
+                      to="/dashboard"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                      role="menuitem"
+                    >
+                      Dashboard
+                    </Link>
+                    <button
+                    onClick={() => nhost.auth.signOut()}
+                      className="text-center w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                      role="menuitem"
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </div>
+              )}
             </button>
           </div>
         </div>
